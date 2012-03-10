7c610a35b2d0be9c2ddddc600d20659fe0468f52
Mikhail Stolbov <mstolbov@gmail.com>
2012-03-10 15:48:58 +0400

    fxed models

--- models.js
+++ models.js
@@ -3,18 +3,16 @@ var mWindow = function(options) { this.init(options); }
 
 $.extend(mWindowPart.prototype, {
     init: function(options) {
-        this.type = options.type;
-        this.material = options.material;
+        this.type = options.type || "";
+        this.material = options.material || "";
         this.width = options.width || 700;
         this.height = options.height || 1500;
-        return true;
     }
 });
 
 $.extend(mWindow.prototype, {
     init: function(options) {
-        this.parts = options.parts;
-        this.price = options.price;
-        return true;
+        this.parts = options.parts || [];
+        this.price = options.price || 0;
     }
 });
