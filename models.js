ae18367d4ba7c02966e62fe01cfd9999aa9fe464
Mikhail Stolbov <mstolbov@gmail.com>
2012-03-10 15:08:53 +0400

    fixed models

--- models.js
+++ models.js
@@ -1,8 +1,20 @@
+var mWindowPart = function(options) { this.init(options); }
 var mWindow = function(options) { this.init(options); }
 
-$.extend(mWindow.prototype, {
+$.extend(mWindowPart.prototype, {
     init: function(options) {
         this.type = options.type;
         this.material = options.material;
+        this.width = options.width || 700;
+        this.height = options.height || 1500;
+        return true;
+    }
+});
+
+$.extend(mWindow.prototype, {
+    init: function(options) {
+        this.parts = options.parts;
+        this.price = options.price;
+        return true;
     }
 });
